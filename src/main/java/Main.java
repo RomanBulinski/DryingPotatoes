14941af7e287851acef07dba60007d69eb97f14b
Roman Bulinski <aboandrus@gmail.com>
2019-04-30 18:58:09 +0200

    Implemented code

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+//    potatoes(99, 100, 98) --> 50
+
+    public static int potatoes(int p0, int w0, int p1) {
+        return ((p0 * w0) *100)/p1;
+    }
+
+
+}
---
+++ src/test/java/TestMainClass.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class TestMainClass {
+
+    @Test
+    public void test1(){
+        assertEquals(50, Main.potatoes(99,100,98));
+    }
+
+}
+
+
+
+
+
+
